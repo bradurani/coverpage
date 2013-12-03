8f0f3a1aa1dfafe12c3a1dc8ec224ff923b7872e
Brad Urani <bradurani@gmail.com>
2013-12-03 12:15:46 -0600

    Changes from Monday

--- coverpage/App_Start/RouteConfig.cs
+++ coverpage/App_Start/RouteConfig.cs
@@ -11,12 +11,25 @@ namespace coverpage
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           // routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "AccountRoute",
+                url: "members/account/{action}",
+                defaults: new { controller = "Account", action = "Index" }
+           );
+
+            routes.MapRoute(
+                name: "ArticleRoute",
+                url: "content/article",
+                defaults: new { controller = "Home", action = "Article" }
+            );
+
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{action}/",
+                defaults: new { controller = "Home", action = "MostViewed" }
             );
         }
     }
---
+++ coverpage/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using coverpage.Models;
+
+namespace coverpage.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(SignInModel model)
+        {
+            bool isValid = IsValidUser(model);
+            if (!isValid)
+            {
+                ModelState.AddModelError("", "Your username or password is invalid");
+                return View(model);
+            }
+            else //is a valid user
+                return RedirectToAction("MostViewed", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("MostViewed", "Home");
+            }
+            return View(model);
+        }
+
+        private bool IsValidUser(SignInModel model)
+        {
+            //DATABASE WOULD DO THIS NORMALLY
+            if(model.UserName == "brad" && model.Password == "password")
+                return true;
+            return false;
+        }
+
+    }
+}
